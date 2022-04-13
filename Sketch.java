5ed5cdc78456f53b11c6b953d6823de9e7b94a59
braydenfungg <brayden.fung23@ycdsbk12.ca>
2022-04-12 20:52:59 -0400

    done

--- Sketch.java
+++ Sketch.java
@@ -8,7 +8,7 @@ public class Sketch extends PApplet {
    */
   public void settings() {
 	// put your size call here
-    size(400, 400);
+    size(400, 500);
   }
 
   /** 
@@ -16,21 +16,85 @@ public class Sketch extends PApplet {
    * values here i.e background, stroke, fill etc.
    */
   public void setup() {
-    background(210, 255, 173);
+    background(50, 50, 200);
   }
 
   /**
    * Called repeatedly, anything drawn to the screen goes here
    */
   public void draw() {
-	  
-	// sample code, delete this stuff
-    stroke(128);
-    line(150, 25, 270, 350);  
 
-    stroke(255);
-    line(50, 125, 70, 50);  
+    house();
+    sun(400,0);
+    sign();
+    grass();
   }
+	
+  public void house()
+  {
+    //Stroke settings for the body of the house
+    fill(129, 128, 128);
+    stroke(4);
+
+    //Drawing the house
+    rect(100, 200, 200, 200);
+
+    //Stroke settings for the door
+    fill(255, 100, 100);
+    stroke(200);
+
+    //Drawing the door
+    rect(125, 300, 50, 100);
+
+    //Stroke settings for the roof
+    fill(110, 110, 110);
+    stroke(0);
+
+    //Drawing the roof
+    triangle(100, 200, 200, 100, 300, 200);
+
+    //Stroke settings for window
+    fill(225, 225, 225);
+
+    //Drawing the window
+    rect(190, 245, 100, 50);
+  }
+
+  public void grass()
+  {
+    //Stroke settings for grass 
+    fill(100,225,100);
+    stroke(0);
+
+    //Drawing the grass
+    rect(0, 400, 1000, 1000);
+  }
+   
+  public void sun(int sunX, int sunY)
+  {
+    //Stroke settings for the sun
+    fill(255, 255, 0);
+    stroke(4);
+
+    //Drawing the sun
+    ellipse(400, 0, 100, 100);
+  }
+  
+  public void sign()
+  {
+    //Stroke settings for sign pole
+    stroke(0);
+
+    //Drawing the pole
+    line(50, 460, 50, 350);
+
+    //Stoke settings for the sign body
+    fill(100, 200, 200);
+
+    //Drawing the sign body
+    rect(25, 350, 50, 25);
+
+  }  
   
   // define other methods down here.
 }
